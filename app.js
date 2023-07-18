be004a338001728e7d7b10a63eb14458c24a5d4a
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-18 16:15:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const express = require("express");
 
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  res.status(201).json([{username, password}]);
 });
 
 
