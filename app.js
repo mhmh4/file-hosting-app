631b5f6b365912d6c2abe9aa41b9d68715df32c9
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-18 14:57:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
 
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render("index");
 });
+
+
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
 
 app.listen(port);
