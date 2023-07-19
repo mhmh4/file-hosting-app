1a864f435c9fd79d42d3d7f1e728fc1316623084
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-19 08:46:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
-const sqlite3 = require("sqlite3");
-const { open } = require("sqlite");
 
-let db = null;
-// this is a top-level await
-(async () => {
-  // open the database
-  db = await open({
-    filename: "db.sqlite3",
-    driver: sqlite3.Database,
-  });
-  return db;
-})();
+const User = require("./models/user");
+const db = require("./db");
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.redirect("login");
-  // res.render("index");
 });
 
 app.get("/register", async (req, res) => {
@@ -34,27 +24,45 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req, res) => {
-  res.redirect("home");
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const user = await User.find({ username: username });
+  console.log(user);
+  if (user != []) {
+    res.redirect("home");
+  } else {
+    res.redirect("login");
+  }
 });
-
 app.get("/home", (req, res) => {
   res.render("home");
 });
 
 app.post("/register", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  // const username = req.body.username;
+  // const password = req.body.password;
+  // try {
+  //   const salt = await bcrypt.genSalt();
+  //   const hashedPassword = await bcrypt.hash(password, salt);
+  //   await db.run(
+  //     `INSERT INTO users (username, password) VALUES ("${username}", "${hashedPassword}")`,
+  //   );
+  // } catch {
+  //   res.status(500);
+  // }
+  // res.status(201).json([{username, password}]);
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
   try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-    await db.run(
-      `INSERT INTO users (username, password) VALUES ("${username}", "${hashedPassword}")`,
-    );
+    const newUser = await user.save();
   } catch {
     res.status(500);
   }
-  // res.status(201).json([{username, password}]);
+
   res.redirect("login");
 });
 
@@ -65,9 +73,5 @@ app.post("/upload", (req, res) => {
 app.get("/logout", (req, res) => {
   res.redirect("login");
 });
-
-// const userRouter = require("./routes/users");
-
-// app.use("/users", userRouter);
 
 app.listen(port);
