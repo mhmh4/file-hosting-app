d72a1323bc74cf4be53a91b01cbe7d36f8cd1bcf
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-19 12:53:33 -0400

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
   username: { type: String },
   password: { type: String },
+  files: { type: [String] },
 });
 
 module.exports = mongoose.model("User", schema);
