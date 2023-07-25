afc76b6f6755c2bc1b07551913c1a6cfa6fde04a
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-25 00:04:35 -0400

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/fhsp");
-
-const db = mongoose.connection;
-
-db.on("error", (error) => console.log(error));
-
-module.exports = db;
