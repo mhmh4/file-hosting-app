7b2c3082970127126786df1415980daa70a03e80
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-06 21:26:53 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -15,30 +15,20 @@ router.get("/login", (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    if (!user) {
+    if (!user || req.body.password !== user.password) {
       req.flash("info", "Invalid username or password");
-      res.redirect("/login");
-      return;
+      return res.redirect("/login");
     }
-
-    if (req.body.password === user.password) {
-      req.session.username = user.username;
-    } else {
-      req.flash("info", "Invalid username or password");
-      res.redirect("/login");
-      return;
-    }
-  } catch {
-    req.flash("info", "Error: Login failed. Please try again.");
-    res.redirect("/login");
-    return;
+    req.session.username = user.username;
+  } catch (error) {
+    req.flash("info", "Something went wrong. Please try again.");
+    return res.redirect("/login");
   }
 
-  res.redirect("/home");
-  return;
+  return res.redirect("/home");
 });
 
-router.get("/register", async (req, res) => {
+router.get("/register", (req, res) => {
   res.render("register.html");
 });
 
@@ -47,19 +37,19 @@ router.post("/register", async (req, res) => {
     const existingUser = await User.findOne({ username: req.body.username });
     if (existingUser) {
       req.flash("info", "Username is already taken");
-    } else {
-      const newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-      });
-      await newUser.save();
-      req.flash("info", "Account created. You may now sign in.");
-      return res.redirect("/login");
+      return res.redirect("/register");
     }
+    await new User({
+      username: req.body.username,
+      password: req.body.password,
+    }).save();
   } catch {
-    req.flash("info", "Error: Registration failed. Please try again.");
+    req.flash("info", "Something went wrong. Please try again.");
+    return res.redirect("/register");
   }
-  res.redirect("/register");
+
+  req.flash("info", "Account created. You may now sign in.");
+  return res.redirect("/login");
 });
 
 router.post("/logout", (req, res) => {
